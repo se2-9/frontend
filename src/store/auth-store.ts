0f401453058572bab0e4c737dfb34d32cd654357
Gear <macgeargear@gmail.com>
2025-01-31 01:02:57 +0700

    feat: new auth store

--- src/store/auth-store.ts
+++ src/store/auth-store.ts
@@ -1,55 +1,67 @@
-import { getUserProfile, logout } from '@/lib/api/auth';
+import { getUserProfile, logout, refreshAccessToken } from '@/lib/api/auth';
 import { User } from '@/types/user';
 import { DtoToUser } from '@/utils/mapper/user-mapper';
 import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
 
 type AuthState = {
   user: User | null;
-  setUser: (user: User) => void;
   accessToken: string | null;
-  refreshToken: string | null;
-  setToken: (accessToken: string, refreshToken: string) => void;
-  fetchUser: () => Promise<void>;
   isAuth: boolean;
+  login: (token: string, user: User) => void;
   logout: () => Promise<void>;
+  fetchUser: () => Promise<void>;
+  refreshAccessToken: () => Promise<void>;
 };
 
-export const useAuthStore = create<AuthState>()(
-  persist(
-    (set, get) => ({
-      user: null,
-      accessToken: null,
-      refreshToken: null,
-      isAuth: false,
-      setUser: (user) => set({ user }),
-      setToken: (accessToken, refreshToken) =>
-        set({ accessToken, refreshToken }),
-      logout: async () => {
-        const refreshToken = get().refreshToken;
-        if (!refreshToken) return;
+export const useAuthStore = create<AuthState>((set, get) => ({
+  user: null,
+  accessToken: null,
+  isAuth: false,
 
-        try {
-          await logout();
-        } catch (error) {
-          console.error('logout failed: ', error);
-        }
-        set({ user: null, accessToken: null, refreshToken: null });
-        localStorage.removeItem('auth-store');
-      },
-      fetchUser: async () => {
-        try {
-          const res = await getUserProfile();
+  login: (token, user) => {
+    set({ user, accessToken: token, isAuth: true });
+  },
 
-          set({ user: DtoToUser(res.result!), isAuth: true });
-        } catch (error) {
-          console.error(error);
-          set({ user: null, isAuth: false });
-        }
-      },
-    }),
-    {
-      name: 'auth-storage',
+  logout: async () => {
+    if (!get().isAuth) return;
+
+    set({ user: null, accessToken: null, isAuth: false });
+
+    try {
+      await logout();
+      set({ user: null, accessToken: null, isAuth: false });
+    } catch (error) {
+      console.error(error);
     }
-  )
-);
+
+    set({ user: null, accessToken: null, isAuth: false });
+  },
+
+  fetchUser: async () => {
+    if (!get().isAuth) return;
+
+    try {
+      const res = await getUserProfile();
+
+      set({ user: DtoToUser(res.result!), isAuth: true });
+    } catch (error) {
+      console.error(error);
+      set({ user: null, isAuth: false });
+    }
+  },
+
+  refreshAccessToken: async () => {
+    if (!get().isAuth) return;
+
+    try {
+      const response = await refreshAccessToken();
+      const accessToken = response.data.result.access_token;
+      set({ accessToken, isAuth: true });
+    } catch (error) {
+      console.error(error);
+
+      if (!get().isAuth) return;
+      get().logout();
+    }
+  },
+}));

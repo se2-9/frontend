f7e112730b5af84722052613ccd57301f1df0699
Gear <macgeargear@gmail.com>
2025-02-02 17:24:31 +0700

    fix: update ui

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,15 +1,40 @@
 import LoginForm from '@/components/auth/login-form';
 import MaxWidthWrapper from '@/components/max-width-wrapper';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
 
 export default function Page() {
   return (
-    <MaxWidthWrapper className="w-full h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-semibold text-center mb-10">
-        อย่าหาว่าพี่สอน
-      </h1>
-      <div className="w-full max-w-md">
-        <LoginForm />
-      </div>
+    <MaxWidthWrapper className="w-full h-screen flex flex-col items-center justify-center text-text">
+      <Card className="w-full max-w-md bg-background py-2">
+        <CardHeader className="px-10">
+          <p className="text-xl mb-2 font-medium">Welcome !</p>
+          <CardTitle className="text-2xl">Sign in to</CardTitle>
+          <CardDescription className="font-bold">
+            อย่าหาว่าพี่สอน
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <LoginForm />
+        </CardContent>
+        <CardFooter className="px-10">
+          <Link
+            className={cn('w-full text-center hover:underline mt-10 text-sm')}
+            href="/register"
+          >
+            Don&apos;t have an account?{'  '}
+            <span className="font-bold hover:underline">Register</span>
+          </Link>
+        </CardFooter>
+      </Card>
     </MaxWidthWrapper>
   );
 }

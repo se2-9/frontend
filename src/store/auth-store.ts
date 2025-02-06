ff1ddee00796304b7f552c19e1f9ba30e9c77645
Gear <macgeargear@gmail.com>
2025-02-06 12:21:41 +0700

    fix: use persist in auth-store, remove auth-provider

--- src/store/auth-store.ts
+++ src/store/auth-store.ts
@@ -1,5 +1,6 @@
 import { DtoToUser } from '@/utils/mapper/user-mapper';
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import { apiClient } from '@/lib/api/axios';
 import { User } from '@/types/user';
 import { refreshAccessToken } from '@/lib/api/auth';
@@ -11,42 +12,59 @@ interface AuthState {
   accessToken: string | null;
   setAuth: (token: string | null, user: User | null) => void;
   initializeAuth: () => Promise<void>;
-  logout: () => void;
+  logout: () => Promise<void>;
 }
 
-export const useAuthStore = create<AuthState>((set) => ({
-  accessToken: null,
-  user: null,
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      accessToken: null,
+      user: null,
 
-  setAuth: (token, user) => set({ accessToken: token, user }),
+      setAuth: (token, user) => {
+        apiClient.defaults.headers['Authorization'] = token
+          ? `Bearer ${token}`
+          : '';
+        set({ accessToken: token, user });
+      },
 
-  initializeAuth: async () => {
-    try {
-      const response = await refreshAccessToken();
-      const accessToken = response.result?.access_token;
-      apiClient.defaults.headers['Authorization'] = `Bearer ${accessToken}`;
+      initializeAuth: async () => {
+        try {
+          const response = await refreshAccessToken();
+          const accessToken = response.result?.access_token;
 
-      const userProfileResp = await apiClient.get<ApiResponse<UserDTO>>(
-        '/auth/me',
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
+          if (!accessToken) throw new Error('No access token received');
+
+          apiClient.defaults.headers['Authorization'] = `Bearer ${accessToken}`;
+
+          const userProfileResp =
+            await apiClient.get<ApiResponse<UserDTO>>('/auth/me');
+
+          set({
+            accessToken: accessToken,
+            user: DtoToUser(userProfileResp.data.result!),
+          });
+        } catch (error) {
+          console.error(
+            'Failed to refresh token, user needs to login: ',
+            error
+          );
+          set({ accessToken: null, user: null });
         }
-      );
+      },
 
-      set({
-        accessToken: accessToken,
-        user: DtoToUser(userProfileResp.data.result!),
-      });
-    } catch (error) {
-      console.error('Failed to refresh token, user needs to login: ', error);
-      set({ accessToken: null, user: null });
+      logout: async () => {
+        try {
+          await apiClient.post('/auth/logout');
+        } catch (error) {
+          console.error('Logout error:', error);
+        } finally {
+          set({ accessToken: null, user: null });
+        }
+      },
+    }),
+    {
+      name: 'auth-storage', // LocalStorage Key
     }
-  },
-
-  logout: async () => {
-    await apiClient.post('/auth/logout');
-    set({ accessToken: null });
-  },
-}));
+  )
+);

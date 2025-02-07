6f96a9f16f5659bb64980e060c26b77c8768a028
Tantorn Dhippayom <tantornd@gmail.com>
2025-02-07 20:53:15 +0700

    change mypost to side

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,4 +1,4 @@
-import LoginForm from '@/components/auth/login-form';
+/*import LoginForm from '@/components/auth/login-form';
 import MaxWidthWrapper from '@/components/max-width-wrapper';
 import {
   Card,
@@ -35,6 +35,31 @@ export default function Page() {
           </Link>
         </CardFooter>
       </Card>
+    </MaxWidthWrapper>
+  );
+}*/
+'use client';
+import { useState } from 'react';
+import CreatePost from '@/components/create-post';
+import MyPosts from '@/components/my-post';
+import MaxWidthWrapper from '@/components/max-width-wrapper';
+
+type Post = {
+  id: string;
+  title: string;
+  HourlyRate: number;
+  OnlineOnsite: string;
+};
+
+export default function Page() {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  return (
+    <MaxWidthWrapper className="w-full h-screen flex flex-col items-center justify-center">
+      <div className="w-full max-w-md mt-[144px]">
+        <CreatePost />
+        <MyPosts posts={posts} setPosts={setPosts} />
+      </div>
     </MaxWidthWrapper>
   );
 }

9e6e87506ca1f3e3c0f684031e3564555371eb36
Gear <macgeargear@gmail.com>
2025-02-13 19:50:01 +0700

    feat: gender icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -69,4 +69,75 @@ export const Icons = {
       />
     </svg>
   ),
+  male: (props: LucideProps) => (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="lucide lucide-mars"
+    >
+      <path d="M16 3h5v5" />
+      <path d="m21 3-6.75 6.75" />
+      <circle
+        cx="10"
+        cy="14"
+        r="6"
+      />
+    </svg>
+  ),
+  female: (props: LucideProps) => (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="lucide lucide-venus"
+    >
+      <path d="M12 15v7" />
+      <path d="M9 19h6" />
+      <circle
+        cx="12"
+        cy="9"
+        r="6"
+      />
+    </svg>
+  ),
+  any: (props: LucideProps) => (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="lucide lucide-venus-and-mars"
+    >
+      <path d="M10 20h4" />
+      <path d="M12 16v6" />
+      <path d="M17 2h4v4" />
+      <path d="m21 2-5.46 5.46" />
+      <circle
+        cx="12"
+        cy="11"
+        r="5"
+      />
+    </svg>
+  ),
 };

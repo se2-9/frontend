49c5d1f82a3e38acfde75d687622b911438b5a6b
Gear <macgeargear@gmail.com>
2025-02-15 01:33:30 +0700

    chore

--- src/components/profile/edit-profile-form.tsx
+++ src/components/profile/edit-profile-form.tsx
@@ -1,45 +1,55 @@
-import { editUserProfile } from "@/lib/api/profile";
-import { useAuthStore } from "@/store/auth-store";
-import { DtoToUser } from "@/utils/mapper/user-mapper";
-import { useMutation } from "@tanstack/react-query";
-import { EyeOffIcon, EyeIcon } from "lucide-react";
-import { useForm } from "react-hook-form";
-import { toast } from "sonner";
-import { Button } from "../ui/button";
-import { Form, FormField, FormItem, FormLabel, FormControl, FormDescription } from "../ui/form";
-import { Input } from "../ui/input";
-import { EditUserProfileFormRequest, EditUserProfileFormSchema } from "@/lib/validations/profile";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useState, useEffect } from "react";
-import { Textarea } from "../ui/textarea";
+import { editUserProfile } from '@/lib/api/profile';
+import { useAuthStore } from '@/store/auth-store';
+import { DtoToUser } from '@/utils/mapper/user-mapper';
+import { useMutation } from '@tanstack/react-query';
+import { EyeOffIcon, EyeIcon } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+import { Button } from '../ui/button';
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+} from '../ui/form';
+import { Input } from '../ui/input';
+import {
+  EditUserProfileFormRequest,
+  EditUserProfileFormSchema,
+} from '@/lib/validations/profile';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useState, useEffect } from 'react';
+import { Textarea } from '../ui/textarea';
 
 export default function EditProfileForm() {
   const [showPassword, setShowPassword] = useState(true);
   const currentUser = useAuthStore((state) => state.user);
   const setAuth = useAuthStore((state) => state.setAuth);
-  console.log(currentUser)
+  console.log(currentUser);
   const form = useForm<EditUserProfileFormRequest>({
     resolver: zodResolver(EditUserProfileFormSchema),
     defaultValues: {
-      password: "",
-      name: "",
-      tutor_education_level: "",
-      tutor_portfolio: "",
+      password: '',
+      name: '',
+      tutor_education_level: '',
+      tutor_portfolio: '',
       date_of_birth: currentUser?.dateOfBirth.split('T')[0],
-      citizen_id: "",
-      phone_number: ""
+      citizen_id: '',
+      phone_number: '',
     },
   });
 
   useEffect(() => {
     if (currentUser) {
       form.reset({
-        name: currentUser.name || "",
-        tutor_education_level: currentUser.tutorEducationLevel || "",
-        tutor_portfolio: currentUser.tutorPortfolio || "",
-        date_of_birth: currentUser.dateOfBirth.split('T')[0] || "",
-        citizen_id: currentUser.citizenId || "",
-        phone_number: currentUser.phoneNumber || "0000000000",
+        name: currentUser.name || '',
+        tutor_education_level: currentUser.tutorEducationLevel || '',
+        tutor_portfolio: currentUser.tutorPortfolio || '',
+        date_of_birth: currentUser.dateOfBirth.split('T')[0] || '',
+        citizen_id: currentUser.citizenId || '',
+        phone_number: currentUser.phoneNumber || '0000000000',
       });
     }
   }, []);
@@ -48,16 +58,20 @@ export default function EditProfileForm() {
     mutationFn: editUserProfile,
     onSuccess: (data) => {
       if (!data.result) {
-        toast.error("Something went wrong");
+        toast.error('Something went wrong');
         return;
       }
       try {
-        const currUser = data.result
-        setAuth(useAuthStore.getState().accessToken, useAuthStore.getState().expiresAt,DtoToUser(currUser));
-        toast.success("Updated");
+        const currUser = data.result;
+        setAuth(
+          useAuthStore.getState().accessToken,
+          useAuthStore.getState().expiresAt,
+          DtoToUser(currUser)
+        );
+        toast.success('Updated');
       } catch (error) {
         console.error(error);
-        toast.error("Something went wrong");
+        toast.error('Something went wrong');
       }
     },
     onError: (err) => toast.error(err.message),
@@ -65,23 +79,25 @@ export default function EditProfileForm() {
 
   function onSubmit(values: EditUserProfileFormRequest) {
     mutation.mutate({
-        role: currentUser?.role || "student",
-        email: currentUser?.email || "myemail.gmail.com",
-        password: values.password,
-        date_of_birth: values.date_of_birth+"T00:00:00+07:00",
-        tutor_education_level: values.tutor_education_level,
-        tutor_portfolio: values.tutor_portfolio,
-        verify_status: true,
-        citizen_id: values.citizen_id,
-        name: values.name,
-        phone_number: values.phone_number,
-        gender: currentUser?.gender || "male"
-    })
+      role: currentUser?.role || 'student',
+      email: currentUser?.email || 'myemail.gmail.com',
+      password: values.password,
+      date_of_birth: values.date_of_birth + 'T00:00:00+07:00',
+      tutor_education_level: values.tutor_education_level,
+      tutor_portfolio: values.tutor_portfolio,
+      verify_status: true,
+      citizen_id: values.citizen_id,
+      name: values.name,
+      phone_number: values.phone_number,
+      gender: currentUser?.gender || 'male',
+    });
   }
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} 
-      className="space-y-2 w-full mx-auto px-4 text-text">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-2 w-full mx-auto px-4 text-text"
+      >
         <FormField
           name="name"
           render={({ field }) => (
@@ -112,7 +128,7 @@ export default function EditProfileForm() {
                   <Input
                     {...field}
                     placeholder="New Password"
-                    type={showPassword ? "text" : "password"}
+                    type={showPassword ? 'text' : 'password'}
                   />
                   <Button
                     type="button"
@@ -120,7 +136,11 @@ export default function EditProfileForm() {
                     className="bg-background text-text h-fit absolute right-2 top-1/2 -translate-y-1/2 p-1"
                     onClick={() => setShowPassword((prev) => !prev)}
                   >
-                    {showPassword ? <EyeOffIcon className="h-5 w-5" /> : <EyeIcon className="h-5 w-5" />}
+                    {showPassword ? (
+                      <EyeOffIcon className="h-5 w-5" />
+                    ) : (
+                      <EyeIcon className="h-5 w-5" />
+                    )}
                   </Button>
                 </div>
               </FormControl>
@@ -131,49 +151,52 @@ export default function EditProfileForm() {
           )}
           control={form.control}
         />
-        {currentUser?.role == "tutor" &&<FormField
-          name="tutor_education_level"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Education Level</FormLabel>
-              <FormControl>
-                {/* <Input
+        {currentUser?.role == 'tutor' && (
+          <FormField
+            name="tutor_education_level"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Education Level</FormLabel>
+                <FormControl>
+                  {/* <Input
                   {...field}
                   placeholder="Enter education level"
                   type="text"
                 /> */}
-                <Textarea 
-                {...field}
-                placeholder="Enter education level"
-                />
-              </FormControl>
-              <FormDescription className="text-destructive">
-                {form.formState.errors.tutor_education_level?.message}
-              </FormDescription>
-            </FormItem>
-          )}
-          control={form.control}
-        />}
-        
-        {currentUser?.role == "tutor" && <FormField
-          name="tutor_portfolio"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Portfolio</FormLabel>
-              <FormControl>
-                <Textarea 
-                {...field}
-                placeholder="Enter portfolio details"
-                />
-              </FormControl>
-              <FormDescription className="text-destructive">
-                {form.formState.errors.tutor_portfolio?.message}
-              </FormDescription>
-            </FormItem>
-          )}
-          control={form.control}
-        />}
-        
+                  <Textarea
+                    {...field}
+                    placeholder="Enter education level"
+                  />
+                </FormControl>
+                <FormDescription className="text-destructive">
+                  {form.formState.errors.tutor_education_level?.message}
+                </FormDescription>
+              </FormItem>
+            )}
+            control={form.control}
+          />
+        )}
+
+        {currentUser?.role == 'tutor' && (
+          <FormField
+            name="tutor_portfolio"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Portfolio</FormLabel>
+                <FormControl>
+                  <Textarea
+                    {...field}
+                    placeholder="Enter portfolio details"
+                  />
+                </FormControl>
+                <FormDescription className="text-destructive">
+                  {form.formState.errors.tutor_portfolio?.message}
+                </FormDescription>
+              </FormItem>
+            )}
+            control={form.control}
+          />
+        )}
 
         <FormField
           name="date_of_birth"
@@ -184,7 +207,7 @@ export default function EditProfileForm() {
                 <Input
                   {...field}
                   type="date"
-                  value={field.value ? field.value:""}
+                  value={field.value ? field.value : ''}
                   onChange={(e) => field.onChange(e.target.value)}
                 />
               </FormControl>
@@ -203,7 +226,7 @@ export default function EditProfileForm() {
               <FormControl>
                 <Input
                   {...field}
-                  placeholder= "Enter your citizen ID"
+                  placeholder="Enter your citizen ID"
                   type="text"
                 />
               </FormControl>
@@ -233,7 +256,10 @@ export default function EditProfileForm() {
           )}
           control={form.control}
         />
-        <Button className="w-full text-text bg-lightbrown" type="submit">
+        <Button
+          className="w-full text-text bg-lightbrown"
+          type="submit"
+        >
           Save Profile
         </Button>
       </form>

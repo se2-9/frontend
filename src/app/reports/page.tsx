a3189e057a43af7cf4bac16d75a0a175726bced7
Snowfall <atittarn2@gmail.com>
2025-03-11 14:23:57 +0700

    Remove unused things

--- src/app/reports/page.tsx
+++ src/app/reports/page.tsx
@@ -1,52 +1,16 @@
 'use client';
 
-import { useMemo } from 'react';
 import { useAuthStore } from '@/store/auth-store';
 import MaxWidthWrapper from '@/components/max-width-wrapper';
 import { ProfileCard } from '@/components/profile/profile-card';
 import { Breadcrumbs } from '@/components/ui/breadcrumbs';
 import AvatarDropdownProfile from '@/components/profile/avatar-dropdown-profile';
 import { ReportsTable } from '@/components/reports/reports-table';
-import { ReportDTO } from '@/dtos/report';
 import { useReports } from '@/hooks/useReports';
 import { Icons } from '@/components/icons';
 
 export default function ReportsPage() {
   const user = useAuthStore((state) => state.user);
-
-  const reportsFromUser: ReportDTO[] = useMemo(
-    () => [
-      {
-        report_id: '1',
-        user_id: 'John Doe',
-        content: 'Test 1',
-        created_at: '2024-02-01',
-        status: 'pending',
-      },
-      {
-        report_id: '2',
-        user_id: 'Alice Smith',
-        content: 'Test 2',
-        created_at: '2024-01-29',
-        status: 'not paid',
-      },
-      {
-        report_id: '3',
-        user_id: 'Michael Lee',
-        content: 'Test 3',
-        created_at: '2024-01-27',
-        status: 'resolved',
-      },
-      {
-        report_id: '4',
-        user_id: 'Michael Lee',
-        content: 'Test 4',
-        created_at: '2024-01-27',
-        status: 'rejected',
-      },
-    ],
-    []
-  );
   const { data, isLoading } = useReports();
 
   if (isLoading) {

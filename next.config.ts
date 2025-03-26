4113200035add51b961e5fa1a9f708098c790be9
Gear <macgeargear@gmail.com>
2025-03-27 00:12:44 +0700

    fix: nextconf

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_OMISE_PUBLIC_KEY: process.env.NEXT_PUBLIC_OMISE_PUBLIC_KEY,
+    NEXT_PUBLIC_OMISE_SECRET_KEY: process.env.NEXT_PUBLIC_OMISE_SECRET_KEY,
+    NEXT_PUBLIC_OMISE_API_VERSION: process.env.NEXT_PUBLIC_OMISE_API_VERSION,
+  },
 };
 
 export default nextConfig;

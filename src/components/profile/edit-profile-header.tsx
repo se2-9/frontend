08fd4b55267174e6329a187669bdf08b0f89a0ec
Gear <macgeargear@gmail.com>
2025-04-24 00:15:31 +0700

    chore

--- src/components/profile/edit-profile-header.tsx
+++ src/components/profile/edit-profile-header.tsx
@@ -1,7 +1,13 @@
 import { useAuthStore } from '@/store/auth-store';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 import { Button } from '../ui/button';
-import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle } from '../ui/dialog';
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '../ui/dialog';
 import { useState } from 'react';
 import { Input } from '../ui/input';
 import { useMutation } from '@tanstack/react-query';
@@ -14,16 +20,16 @@ export default function ProfileView() {
   const router = useRouter();
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [confirmationText, setConfirmationText] = useState('');
-  
+
   const mutation = useMutation({
     mutationFn: deleteUser,
     onSuccess: () => {
-      toast.success("Account deleted successfully")
+      toast.success('Account deleted successfully');
       router.push('/login');
     },
     onError: (error) => {
       console.error('Error deleting account:', error);
-      toast.error("Failed to delete account")
+      toast.error('Failed to delete account');
     },
     onSettled: () => {
       setIsDialogOpen(false);
@@ -40,7 +46,7 @@ export default function ProfileView() {
   };
 
   return (
-    <div className='space-y-6'>
+    <div className="space-y-6">
       <div className="flex items-center space-x-4">
         <Avatar>
           <AvatarImage src={user?.email[0]} />
@@ -52,32 +58,43 @@ export default function ProfileView() {
         </div>
       </div>
       <Button
-          type="submit"
-          className="w-full bg-red-500 hover:bg-red-700 text-white"
-          onClick={() => setIsDialogOpen(true)}
-        >
-          Delete Account
+        type="submit"
+        className="w-full bg-red-500 hover:bg-red-700 text-white"
+        onClick={() => setIsDialogOpen(true)}
+      >
+        Delete Account
       </Button>
-      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+      <Dialog
+        open={isDialogOpen}
+        onOpenChange={setIsDialogOpen}
+      >
         <DialogContent>
           <DialogHeader>
             <DialogTitle>Confirm Deletion</DialogTitle>
           </DialogHeader>
-          <p className="select-none">Are you sure you want to delete your account? This action cannot be undone.</p>
-          <p className="select-none"> If you want to continue, please type &apos;Confirm Delete&apos; to continue</p>
+          <p className="select-none">
+            Are you sure you want to delete your account? This action cannot be
+            undone.
+          </p>
+          <p className="select-none">
+            {' '}
+            If you want to continue, please type <b>Confirm Delete</b> to
+            continue
+          </p>
           <Input
             type="text"
-            placeholder="Type 'Confirm Delete' to proceed"
+            placeholder="Type Confirm Delete to proceed"
             value={confirmationText}
             onChange={(e) => setConfirmationText(e.target.value)}
             className="mt-2"
           />
           <DialogFooter>
             <Button onClick={() => setIsDialogOpen(false)}>Cancel</Button>
-            <Button 
-              className="bg-red-500 hover:bg-red-700 text-white" 
-              onClick={handleDelete}>
-                Delete
+            <Button
+              className="bg-red-500 hover:bg-red-700 text-white"
+              onClick={handleDelete}
+            >
+              Delete
             </Button>
           </DialogFooter>
         </DialogContent>
